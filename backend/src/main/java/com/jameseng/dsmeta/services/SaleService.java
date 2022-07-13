9f399e82fc1e4e9f0f5242f64f164ff2f3755f4f
James-Medeiros-Cardoso <james-medeiros@hotmail.com>
2022-07-13 11:34:49 -0300

    Date select

--- backend/src/main/java/com/jameseng/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/jameseng/dsmeta/services/SaleService.java
@@ -1,8 +1,12 @@
 package com.jameseng.dsmeta.services;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.jameseng.dsmeta.entities.Sale;
@@ -10,12 +14,32 @@ import com.jameseng.dsmeta.repositories.SaleRepository;
 
 @Service
 public class SaleService {
-	
+
 	@Autowired
 	private SaleRepository repository;
-	
-	public List<Sale> findSales() {
-		return repository.findAll();
+
+	/*
+	 * public List<Sale> findSales() { return repository.findAll(); }
+	 */
+
+	/*
+	 * public Page<Sale> findSales(Pageable pageable) { return
+	 * repository.findAll(pageable); }
+	 */
+
+	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
+
+		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		// Instant.now() = instante atual
+		// ZoneId.systemDefault() = fuso horário
+
+		// LocalDate min = LocalDate.parse(maxDate); // converte String para localDate
+		// do Java
+		// today.minusDays(365) = data de hoje, mas há um ano atrás
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+
+		return repository.findSales(min, max, pageable);
 	}
 
 }
